90db47016ab625029b5c049bff64084a5f9aac1d
Camilo Galvis <cjgalvis15@gmail.com>
2025-11-24 00:01:33 -0500

    feat: reestructuración del appbar de la pantalla home

--- lib/ui/features/home/presentation/screens/home_screen.dart
+++ lib/ui/features/home/presentation/screens/home_screen.dart
@@ -28,8 +28,6 @@ class HomeScreen extends ConsumerWidget {
     final incomes = ref.watch(incomesProvider);
     final expenses = ref.watch(expensesProvider);
     final themeMode = ref.watch(themeAppProvider);
-    final isDark =
-        ref.read(themeAppProvider.notifier).isDark(context);
 
     return asyncLabels.when(
       loading: () => const LoadingScreen(),
@@ -40,33 +38,16 @@ class HomeScreen extends ConsumerWidget {
         return Scaffold(
           appBar: AppBar(
             toolbarHeight: sizeAppBar,
-            title: Text(
+            title: _GreetingUser(
               '${model.appBar.greeting} ${userSession.name}',
             ),
-            leading: Padding(
-              padding: EdgeInsets.all(padding10),
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                  userSession.photo ?? model.appBar.avatar,
-                ),
-              ),
+            leading: _Avatar(
+              photo: userSession.photo ?? model.appBar.avatar,
             ),
             actions: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.settings_outlined),
-              ),
-              IconButton(
-                icon: Icon(
-                  isDark ? Icons.light_mode : Icons.dark_mode,
-                ),
-                onPressed: () {
-                  ref.read(themeAppProvider.notifier).setTheme(
-                        themeMode == ThemeMode.light
-                            ? ThemeMode.dark
-                            : ThemeMode.light,
-                      );
-                },
+              _SettingsButton(),
+              _ThemeModeButton(
+                themeMode: themeMode,
               ),
             ],
           ),
@@ -106,6 +87,78 @@ class HomeScreen extends ConsumerWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class _GreetingUser extends StatelessWidget {
+  const _GreetingUser(
+    this.greeting,
+  );
+
+  final String greeting;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(greeting);
+  }
+}
+
+class _SettingsButton extends StatelessWidget {
+  const _SettingsButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      icon: Icon(Icons.settings_outlined),
+    );
+  }
+}
+
+class _ThemeModeButton extends ConsumerWidget {
+  const _ThemeModeButton({
+    required this.themeMode,
+  });
+
+  final ThemeMode themeMode;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDark =
+        ref.read(themeAppProvider.notifier).isDark(context);
+
+    return IconButton(
+      icon: Icon(
+        isDark ? Icons.light_mode : Icons.dark_mode,
+      ),
+      onPressed: () {
+        ref.read(themeAppProvider.notifier).setTheme(
+              themeMode == ThemeMode.light
+                  ? ThemeMode.dark
+                  : ThemeMode.light,
+            );
+      },
+    );
+  }
+}
+
+class _Avatar extends StatelessWidget {
+  const _Avatar({
+    required this.photo,
+  });
+
+  final String photo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(padding10),
+      child: CircleAvatar(
+        backgroundImage: NetworkImage(
+          photo,
+        ),
+      ),
     );
   }
 }
